aebe84378a497686fbb3b429f4cacd5b07240fb2
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-08-15 16:00:39 -0400

    changes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const sess = {
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 const hbs = exphbs.create({
   helpers: {
